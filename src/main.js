993ff13bafe856f8943b8888466c105939ea3be8
Kalvin <kvinski@ucsc.edu>
2020-04-19 23:27:33 -0700

    commitfinal

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+/*
+ - paralax scrolling (15)
+ - control rocket after fire (10)
+ - animated ships (15)
+ - new smaller /faster spaceship w/ animated artwork and death sprite sheet + releases projectiles that can hit other ships for more points (30?)
+ - new artwork for all assets (25)
+ - created two original music tracks, which track you hear depends on difficulty (20?)
+ (I know I can't get over 100)
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
